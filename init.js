95fcf7b63692dc00b145b0a8e7fd146ca8a78bb6
Edouard Cunibil <edouard@happyculture.coop>
2017-01-27 00:32:06 +0100

    Create a GreatOrgan library to manage the states and transitions.

--- init.js
+++ init.js
@@ -1,23 +1,7 @@
 
-var play = require('./lib/play');
+var GreatOrgan = require('./lib/go');
+GreatOrgan.run();
 
-// Switch output to organ.
-play.switch(play.intruments.organ);
-
-// Play intro.
-function intro() {
-  var sequence = [
-    ['d', 400],
-    ['e', 400],
-    ['c', 400],
-    ['C', 800],
-    ['G', 800]
-  ];
-  play.sequence(sequence);
-  setTimeout(intro, 5000);
-}
-intro();
-
-// Wait infinitely.
-console.info('Use CTRL-C to quit.');
-(function wait(n) { n = (n+1)%999999; setTimeout(wait.bind(null, n), 1000); })(0);
+// Temporary: halt the organ after 30 seconds.
+// Todo replace by a halt sequence
+setTimeout(GreatOrgan.halt, 30000);
